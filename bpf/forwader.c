6372b8b8606b1e2254eb3c27e6d34bea5ac8de32
Paramoshka <parfenov_ivan_42a@mail.ru>
2025-02-21 11:08:41 +0300

    refactor
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>

--- bpf/forwader.c
+++ bpf/forwader.c
@@ -7,17 +7,26 @@
 
 
 struct forwarding_rule {
-    __u32 source_ip;
-    __u16 source_port;
-    __u32 destination_ip;
-    __u16 destination_port;
+    __u32 src_ip;   // Исходный IP-адрес (в формате network byte order)
+    __u32 src_mask; // Маска подсети источника
+    __u16 src_port; // Исходный порт
+    __u32 dst_ip;   // Адрес назначения
+    __u16 dst_port; // Порт назначения
+};
+
+
+//mask + ip
+struct ipv4_lpm_key {
+    __u32 prefixlen;
+    __be32 addr;
 };
 
 struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 256);
-    __type(key, __u64);  // source_ip + source_port
-    __type(value, struct forwarding_rule);
+        __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+        __type(key, struct ipv4_lpm_key);
+        __type(value, __u32);
+        __uint(map_flags, BPF_F_NO_PREALLOC);
+        __uint(max_entries, 255);
 } forwarding_map SEC(".maps");
 
 
@@ -26,43 +35,23 @@ int forward_traffic(struct __sk_buff *skb) {
     void *data = (void *)(long)skb->data;
     void *data_end = (void *)(long)skb->data_end;
 
-    if (data + sizeof(struct ethhdr) > data_end) {
-        return TC_ACT_SHOT;
-    }
-
     struct ethhdr *eth = data;
-    if (eth->h_proto != __constant_htons(ETH_P_IP)) {
-        return TC_ACT_OK;
+    struct iphdr *ip = data + sizeof(*eth);
+
+    if ((void *)(ip + 1) > data_end) return XDP_DROP;
+
+    struct ipv4_lpm_key key = {
+        .prefixlen = 32,
+        .addr = ip->daddr,
+    };
+    
+
+    __u32 action = bpf_map_lookup_elem(&forwarding_map, &key);
+    if (action) {
+        //todo
     }
 
-    struct iphdr *ip = data + sizeof(struct ethhdr);
-    if ((void *)ip + sizeof(struct iphdr) > data_end) {
-        return TC_ACT_SHOT;
-    }
-
-    __u64 key = ((__u64)ip->saddr << 16) | ((__u64)ip->id);
-    struct forwarding_rule *rule = bpf_map_lookup_elem(&forwarding_map, &key);
-    if (!rule) {
-        return TC_ACT_OK;
-    }
-
-    ip->daddr = rule->destination_ip;
-
-    if (ip->protocol == IPPROTO_TCP) {
-        struct tcphdr *tcp = (void *)ip + sizeof(struct iphdr);
-        if ((void *)tcp + sizeof(struct tcphdr) > data_end) {
-            return TC_ACT_SHOT;
-        }
-        tcp->dest = rule->destination_port;
-    } else if (ip->protocol == IPPROTO_UDP) {
-        struct udphdr *udp = (void *)ip + sizeof(struct iphdr);
-        if ((void *)udp + sizeof(struct udphdr) > data_end) {
-            return TC_ACT_SHOT;
-        }
-        udp->dest = rule->destination_port;
-    }
-
-    return TC_ACT_OK;
+    return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
